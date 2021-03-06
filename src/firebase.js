114c50607ae3d2a090c2bfd1f805c2ef585b3320
silvioquitto <48890997+silvioquitto@users.noreply.github.com>
2021-03-06 18:06:21 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,12 @@
 import firebase from 'firebase';	
 
 const firebaseConfig = {	
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://PROJECT_ID.firebaseio.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID",
-    measurementId: "G-MEASUREMENT_ID",
+    apiKey: "AIzaSyD690IMYibWP8E2twI8Veg6FcwjZA4VMh0",
+    authDomain: "zapzap-cc461.firebaseapp.com",
+    projectId: "zapzap-cc461",
+    storageBucket: "zapzap-cc461.appspot.com",
+    messagingSenderId: "201088445929",
+    appId: "1:201088445929:web:18b7cf0c723a5db0912f78"
 };	
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);	
